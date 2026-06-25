5df9990c1128f6162659f98bef6ef6c6056ce456
kemal <kemalyen@gmail.com>
2026-06-25 18:49:21 +0100

    fixed

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Console\Commands\BackupTenantDatabases;
 use App\Console\Commands\DemoData;
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
@@ -12,6 +11,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(BackupTenantDatabases::class)->dailyAt('02:00');
 
 Schedule::command(DemoData::class)->dailyAt('04:00');
