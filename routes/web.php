a96e7052e550d8b04556ae5d44d20d92e3258609
kemal <kemalyen@gmail.com>
2026-09-04 15:01:40 +0100

    maryui is added

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,3 @@
 <?php
 
-use App\Services\Helper;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/debug', function () {
-    return 'deli' . '.' . parse_url(config('app.url'), PHP_URL_HOST);
-    return Carbon::now()->toDateTimeString();
-})->name('welcome');
-
-
-foreach (config('tenancy.central_domains') as $domain) {
-    Route::domain($domain)->group(function () {
-        Route::get('/', function () {
-            return 'App';
-        })->name('home');
-    });
-}
+Route::livewire('/', 'pages::users.index');
