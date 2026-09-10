bfa870ce263748305212c56f52218be756c0a32d
kemal <kemalyen@gmail.com>
2026-09-10 08:52:09 +0100

    quick fix for jome

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
-Route::livewire('/', 'pages::users.index');
+use Illuminate\Support\Facades\Route;
+
+Route::get('/home', function () {
+    return redirect()->route('tenant.home');
+})->name('tenant.home');
